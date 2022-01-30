324e5268ea848c2117a95d0f3d7ce9fffb82464e
gallipolli <khruangbin183158@protonmail.com>
2022-01-30 00:06:14 -0800

    fixing the node config files.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -8,5 +9,5 @@ app.get("/", function(req, res) {
 
 app.use("/public", express.static("./public"));
 
-app.listen(8080);
-console.log("listening on http://localhost:8080");
+app.listen(port);
+console.log(`listening on http://localhost:${port}`);
